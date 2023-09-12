6b9dc9b18c7af384ee95e1a4b2b472a40373e4f5
James Jang <jhjx92@gmail.com>
2023-09-12 14:22:51 -0400

    finished collections: inventory

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,29 +10,30 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
+    List<String> shoppyList;
     public Inventory(List<String> strings) {
-
+            shoppyList = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        shoppyList = new ArrayList<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        shoppyList.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        shoppyList.remove(item);
     }
 
     /**
@@ -39,6 +41,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return shoppyList.size();
     }
 }
